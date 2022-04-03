79a882f920815d52c531757a03bf41b54851ff05
WilliamGBrito <will-dragoesdareal@hotmail.com>
2022-04-03 20:55:59 -0300

    Implementing the CQRS pattern in the Application layer

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -4,9 +4,11 @@ using CleanArchMvc.Application.Services;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
 using CleanArchMvc.Infra.Data.Repositories;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 public static class DependencyInjection
 {
@@ -23,6 +25,9 @@ public static class DependencyInjection
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<ICategoryService, CategoryService>();
         services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+        var myhandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+        services.AddMediatR(myhandlers);
 
         return services;
     }
