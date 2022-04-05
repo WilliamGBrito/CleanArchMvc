ac8611e6e0262dedfdf8f159522763c3b4037d04
WilliamGBrito <will-dragoesdareal@hotmail.com>
2022-04-05 17:34:03 -0300

    Implementing security

---
+++ CleanArchMvc.WebUi/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUi.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid format email")]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} anda at max {1} characters long.", MinimumLength = 10)]
+
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
